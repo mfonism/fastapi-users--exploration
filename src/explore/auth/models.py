f7fe665021b670b01539151cf3c37451c1f54a26
Mfon Eti-mfon <mfonetimfon@gmail.com>
2026-01-25 18:34:27 +0000

    Add a user manager

--- src/explore/auth/models.py
+++ src/explore/auth/models.py
@@ -1,7 +1,11 @@
+import uuid
+
 from fastapi import Depends
+from fastapi_users import BaseUserManager, UUIDIDMixin
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .. import settings
 from ..db.base import Base
 from ..db.config import get_async_session
 
@@ -12,3 +16,12 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = settings.RESET_PASSWORD_TOKEN_SECRET
+    verification_token_secret = settings.VERIFICATION_TOKEN_SECRET
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
--- src/explore/settings.py
+++ src/explore/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from pydantic import SecretStr
+
 BASE_DIR = Path(__file__).parent.parent.resolve()
 
 DATA_DIR = BASE_DIR / ".data"
@@ -7,3 +9,6 @@ DATA_DIR.mkdir(exist_ok=True)
 
 CORE_DB_URL = f"sqlite+aiosqlite:///{DATA_DIR / 'db.sqlite'}"
 AUTH_REDIS_URL = "redis://localhost:6379/0"
+
+RESET_PASSWORD_TOKEN_SECRET = SecretStr("EXPLORE: RESET PASSWORD TOKEN SECRET")
+VERIFICATION_TOKEN_SECRET = SecretStr("EXPLORE: VERIFICATION TOKEN SECRET")
